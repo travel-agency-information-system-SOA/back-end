65f0a25e6b7ce530d922635097e36b768048a865
VukPavlovic425 <vuk.pavlovic425@gmail.com>
2023-10-19 21:54:45 +0200

    feat: Guide review crud

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/IGuideReviewService.cs
@@ -0,0 +1,14 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using FluentResults;
+
+namespace Explorer.Tours.API.Public.Administration
+{
+    public interface IGuideReviewService
+    {
+        Result<PagedResult<GuideReviewDto>> GetPaged(int page, int pageSize);
+        Result<GuideReviewDto> Create(GuideReviewDto review);
+        Result<GuideReviewDto> Update(GuideReviewDto review);
+        Result Delete(int id);
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/GuideReview.cs
@@ -0,0 +1,21 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class GuideReview : Entity
+    {
+        public int GuideId {  get; init; }
+        public int Rating { get; init; }
+        public string? Comment { get; init; }
+        public DateTime SubmissionDate { get; init; }
+
+        public GuideReview(int guideId, int rating, string comment, DateTime submissionDate)
+        {
+            if(rating < 1 || rating > 5) throw new ArgumentOutOfRangeException("Invalid Value.");
+            GuideId = guideId;
+            Rating = rating;
+            Comment = comment;
+            SubmissionDate = submissionDate;
+        }
+    }
+}
