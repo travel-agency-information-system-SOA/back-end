405a44a8d3a707093c5a534a70ca4d492177ef1e
Jelena Moraca <moracajelena1@gmail.com>
2023-10-19 19:43:30 +0200

    Create domain problems

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Problem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using AutoMapper.Configuration.Annotations;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Problem:Entity
+    {
+        string Category {  get; init; }
+        string Priority { get; init; }
+        string Description { get; init; }
+       DateTime Time {  get; init; }
+
+        public Problem(string category, string priority, string description, DateTime time)
+        {
+            if (string.IsNullOrWhiteSpace(category)) throw new ArgumentException("Invalid Category.");
+            if (string.IsNullOrWhiteSpace(priority)) throw new ArgumentException("Invalid Priority.");
+            if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Invalid Description.");
+            Category = category;
+            Priority = priority;
+            Description = description;
+            Time = time;
+        }
+    }
+}
