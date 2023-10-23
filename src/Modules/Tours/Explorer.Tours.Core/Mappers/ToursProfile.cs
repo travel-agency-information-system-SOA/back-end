c6b41c6085d2d985ce6c1235cbc63ed8f72c48cf
BrziGolub <petarkovacevic58@gmail.com>
2023-10-23 20:02:43 +0200

    feat: Tourist Preferences CRUD

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -9,5 +9,17 @@ public class ToursProfile : Profile
     public ToursProfile()
     {
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
+
+        CreateMap<PreferencesDto, Preferences>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+            .ForMember(dest => dest.PreferredDifficulty, opt => opt.MapFrom(src => Enum.Parse(typeof(DifficultyLevel), src.PreferredDifficulty)))
+            .ForMember(dest => dest.TransportationPreferences, opt => opt.MapFrom(src => src.TransportationPreferences))
+            .ForMember(dest => dest.InterestTags, opt => opt.MapFrom(src => src.InterestTags));
+        
+        CreateMap<Preferences, PreferencesDto>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+            .ForMember(dest => dest.PreferredDifficulty, opt => opt.MapFrom(src => src.PreferredDifficulty.ToString()))
+            .ForMember(dest => dest.TransportationPreferences, opt => opt.MapFrom(src => src.TransportationPreferences))
+            .ForMember(dest => dest.InterestTags, opt => opt.MapFrom(src => src.InterestTags));
     }
 }
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Marketplace/PreferencesService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Marketplace;
+using FluentResults;
+
+namespace Explorer.Tours.Core.UseCases.Marketplace
+{
+    public class PreferencesService : CrudService<PreferencesDto, Preferences>, IPreferencesService
+    {
+        public PreferencesService(ICrudRepository<Preferences> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
