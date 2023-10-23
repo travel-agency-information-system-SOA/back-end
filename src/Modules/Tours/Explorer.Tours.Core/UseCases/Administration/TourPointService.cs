057bfc27836a9ed8aab8dd10112a393125401936
katarina59 <katarina.medic01@gmail.com>
2023-10-23 23:27:49 +0200

    backend for tour points

---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/TourPointService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Administration
+{
+    public class TourPointService : CrudService<TourPointDto, TourPoint>, ITourPointService
+    {
+        public TourPointService(ICrudRepository<TourPoint> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
