3971f86a44c9acf6a8c492b3ef147f7b146cc603
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-10-25 19:58:34 +0200

    Changes for merge

--- src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/TourPointService.cs
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/TourPointService.cs
@@ -3,6 +3,7 @@ using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
 using Explorer.Tours.Core.Domain;
+using FluentResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,17 @@ namespace Explorer.Tours.Core.UseCases.Administration
     public class TourPointService : CrudService<TourPointDto, TourPoint>, ITourPointService
     {
         public TourPointService(ICrudRepository<TourPoint> repository, IMapper mapper) : base(repository, mapper) { }
-    }
+
+		public Result<PagedResult<TourPointDto>> GetTourPointsByTourId(int tourId)
+		{
+			var allTourPoints = CrudRepository.GetPaged(1, int.MaxValue);
+
+			var filteredTourPoints = allTourPoints.Results.Where(tourPoint => tourPoint.IdTour == tourId);
+
+			var filteredPagedResult = new PagedResult<TourPoint>(filteredTourPoints.ToList(), filteredTourPoints.Count());
+			return MapToDto(filteredPagedResult);
+
+
+		}
+	}
 }
