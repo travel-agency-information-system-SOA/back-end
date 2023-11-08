b2744a6330475113166860353db717bc9153b93a
Dajana00 <dajanaskocajic18@gmail.com>
2023-11-08 20:27:57 +0100

    Izmjene

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/PublicTourObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class PublicTourObject
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string ImageUrl { get; init; }
+        public ObjectCategory Category { get; init; }
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+
+        public PublicTourObject(string name, string description, string imageUrl, ObjectCategory category, double latitude, double longitude)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Category = category;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/PublicTourPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain.Tours
+{
+    public class PublicTourPoint
+    {
+        public int IdTour { get; init; }
+
+        public Tour? Tour { get; init; }
+        public string Name { get; init; }
+        public string? Description { get; init; }
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+        public string ImageUrl { get; init; }
+
+        public PublicTourPoint(int idTour, string name, string? description, double latitude, double longitude, string imageUrl)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Invalid Name.");
+            IdTour = idTour;
+            Name = name;
+            Description = description;
+            Latitude = latitude;
+            Longitude = longitude;
+            ImageUrl = imageUrl;
+        }
+    }
+}
+
