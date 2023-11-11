da821e78455caa24d02c1f9b31df8f6e6856b275
Djoleoo <loncardjole@gmail.com>
2023-11-11 13:39:47 +0100

    Fix: Bug in ТоurExecutionPosition

--- src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutions/TourExecutionPosition.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutions/TourExecutionPosition.cs
@@ -14,5 +14,20 @@ namespace Explorer.Tours.Core.Domain.TourExecutions
         public int Latitude { get; init; }
         public int Longitude { get; init; }
 
+        public TourExecutionPosition(int tourExecutionId, DateTime lastActivity, int latitude, int longitude)
+        {
+            TourExecutionId = tourExecutionId;
+            LastActivity = lastActivity;
+            Latitude = latitude;
+            Longitude = longitude;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (TourExecutionId <= 0)
+                throw new ArgumentException("TourExecutionId must be a positive integer.");
+        }
+
     }
 }
