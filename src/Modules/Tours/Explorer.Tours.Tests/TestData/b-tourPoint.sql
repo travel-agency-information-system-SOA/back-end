fd77afdb588f7bd1bc0181f7dd10495c7b1ad2b7
Dajana00 <dajanaskocajic18@gmail.com>
2023-11-16 11:38:59 +0100

    Gotovo

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/b-tourPoint.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/b-tourPoint.sql
@@ -1,6 +1,6 @@
 INSERT INTO tours."TourPoint"(
-    "Id", "IdTour", "Name", "Description", "Latitude", "Longitude", "ImageUrl")
-VALUES (-1, -1, 'Kej', 'Kej na obali Dunava', 332.3, 7832.32, 'kej.png');
+    "Id", "TourId", "Name", "Description", "Latitude", "Longitude", "ImageUrl","Secret")
+VALUES (-1, -1, 'Kej', 'Kej na obali Dunava', 332.3, 7832.32, 'kej.png','Tajna ture');
 INSERT INTO tours."TourPoint"(
-    "Id", "IdTour", "Name", "Description", "Latitude", "Longitude", "ImageUrl")
-VALUES (-2, -2, 'Srajer', 'Srajer i Nina', 32.3, 832.32, 'frajer.png');
+    "Id", "TourId", "Name", "Description", "Latitude", "Longitude", "ImageUrl","Secret")
+VALUES (-2, -1, 'Petrovaradin', 'Tvrdjavaa', 32.3, 832.32, 'frajer.png','Tajnaaa');
