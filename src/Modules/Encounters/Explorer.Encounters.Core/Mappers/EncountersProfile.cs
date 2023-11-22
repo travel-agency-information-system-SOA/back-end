6423cae85bd2a2d1a59e988cccc9ae8bc820332a
Zela11 <zelovic.ra106.2020@uns.ac.rs>
2023-11-22 03:15:56 +0100

    Added new module Encounters and new class Challenge

---
+++ src/Modules/Encounters/Explorer.Encounters.Core/Mappers/EncountersProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Core.Mappers;
+
+public class EncountersProfile : Profile
+{
+
+}
---
+++ src/Modules/Encounters/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,39 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.BuildingBlocks.Infrastructure.Database;
+using Explorer.Encounters.Core.Mappers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Infrastructure;
+
+public static class EncountersStartup
+{
+    public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(EncountersProfile).Assembly);
+        SetupCore(services);
+        SetupInfrastructure(services);
+        return services;
+    }
+    private static void SetupCore(IServiceCollection services)
+    {
+        //services.AddScoped<IBlogPostService, BlogPostService>();
+    }
+
+    private static void SetupInfrastructure(IServiceCollection services)
+    {
+        /*
+        services.AddScoped(typeof(ICrudRepository<BlogPost>), typeof(CrudDatabaseRepository<BlogPost, BlogContext>));
+
+        services.AddDbContext<BlogContext>(opt =>
+            opt.UseNpgsql(DbConnectionStringBuilder.Build("blog"),
+                x => x.MigrationsHistoryTable("__EFMigrationsHistory", "blog")));
+    */
+    }
+
+}
