ba95b53a56a95a1d54d23cbba1943300f173e709
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-12-27 18:26:40 +0100

    Competition agregate

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/CompetitionApplyDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+	public class CompetitionApplyDto
+	{
+		public int Id { get; set; }
+
+		public long CompetitionId { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public int UserId { get; set; }
+
+		public int NumLikes { get; set; }
+	}
+}
--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -8,6 +8,7 @@ using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.Core.Domain;
 using Explorer.Tours.Core.Domain.Problems;
 using Explorer.Tours.Core.Domain.TourBundle;
+using Explorer.Tours.Core.Domain.Competitions;
 
 namespace Explorer.Tours.Core.Mappers;
 
@@ -30,7 +31,7 @@ public class ToursProfile : Profile
 
 
         CreateMap<PublicTourObjectDto, PublicTourObject>().ReverseMap();
-       
+
 
         CreateMap<TourObjectDto, TourObject>()
         .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
@@ -60,18 +61,18 @@ public class ToursProfile : Profile
      .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
      .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
      .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags))
-	 .ForMember(dest => dest.PublishedDateTime, opt => opt.MapFrom(src => src.PublishedDateTime)); 
+     .ForMember(dest => dest.PublishedDateTime, opt => opt.MapFrom(src => src.PublishedDateTime));
 
 
         CreateMap<Tour, TourDTO>()
-		.ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-		.ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
-		.ForMember(dest => dest.DifficultyLevel, opt => opt.MapFrom(src => src.DifficultyLevel.ToString()))
-		.ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
-		.ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
-	    .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
-		.ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags))
-        .ForMember(dest => dest.PublishedDateTime, opt => opt.MapFrom(src => src.PublishedDateTime)); 
+        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+        .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+        .ForMember(dest => dest.DifficultyLevel, opt => opt.MapFrom(src => src.DifficultyLevel.ToString()))
+        .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+        .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+        .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+        .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags))
+        .ForMember(dest => dest.PublishedDateTime, opt => opt.MapFrom(src => src.PublishedDateTime));
 
 
         CreateMap<TourEquipmentDto, TourEquipment>().ReverseMap();
@@ -85,22 +86,22 @@ public class ToursProfile : Profile
             .ForMember(dest => dest.PreferredDifficulty, opt => opt.MapFrom(src => Enum.Parse(typeof(DifficultyLevel), src.PreferredDifficulty)))
             .ForMember(dest => dest.TransportationPreferences, opt => opt.MapFrom(src => src.TransportationPreferences))
             .ForMember(dest => dest.InterestTags, opt => opt.MapFrom(src => src.InterestTags));
-        
+
         CreateMap<Preferences, PreferencesDto>()
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.PreferredDifficulty, opt => opt.MapFrom(src => src.PreferredDifficulty.ToString()))
             .ForMember(dest => dest.TransportationPreferences, opt => opt.MapFrom(src => src.TransportationPreferences))
             .ForMember(dest => dest.InterestTags, opt => opt.MapFrom(src => src.InterestTags));
 
-		CreateMap<TourCharacteristicDTO, TourCharacteristic>()
-			.ForMember(dest => dest.Distance, opt => opt.MapFrom(src => src.Distance))
-			.ForMember(dest => dest.TransportType, opt => opt.MapFrom(src => Enum.Parse(typeof(DifficultyLevel), src.TransportType)))
-			.ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
+        CreateMap<TourCharacteristicDTO, TourCharacteristic>()
+            .ForMember(dest => dest.Distance, opt => opt.MapFrom(src => src.Distance))
+            .ForMember(dest => dest.TransportType, opt => opt.MapFrom(src => Enum.Parse(typeof(DifficultyLevel), src.TransportType)))
+            .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
 
-		CreateMap<TourCharacteristic, TourCharacteristicDTO>()
-			.ForMember(dest => dest.Distance, opt => opt.MapFrom(src => src.Distance))
-			.ForMember(dest => dest.TransportType, opt => opt.MapFrom(src => src.TransportType.ToString()))
-			.ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
+        CreateMap<TourCharacteristic, TourCharacteristicDTO>()
+            .ForMember(dest => dest.Distance, opt => opt.MapFrom(src => src.Distance))
+            .ForMember(dest => dest.TransportType, opt => opt.MapFrom(src => src.TransportType.ToString()))
+            .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
 
         CreateMap<TourPointExecutionDto, TourPointExecution>().ReverseMap();
 
@@ -108,13 +109,13 @@ public class ToursProfile : Profile
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.TourId, opt => opt.MapFrom(src => src.TourId))
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.Parse(typeof(TourExecutionStatus), src.Status)));
-            
+
 
         CreateMap<TourExecution, TourExecutionDto>()
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.TourId, opt => opt.MapFrom(src => src.TourId))
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
-            
+
 
         CreateMap<TourExecutionPositionDto, TourExecutionPosition>().ReverseMap();
 
@@ -132,5 +133,38 @@ public class ToursProfile : Profile
             .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
             .ForMember(dest => dest.TourIds, opt => opt.MapFrom(src => src.TourIds))
             .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price));
-    }
+
+        CreateMap<Competition, CompetitionDto>()
+        .ForMember(dest => dest.TourId, opt => opt.MapFrom(src => src.TourId))
+        .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.StartDate))
+        .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+        .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
+
+        CreateMap<CompetitionDto, Competition>().IncludeAllDerived()
+
+    .ForMember(dest => dest.TourId, opt => opt.MapFrom(src => src.TourId))
+    .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.StartDate))
+     .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.Parse(typeof(DifficultyLevel), src.Status)))
+
+    .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => src.Duration));
+
+
+		CreateMap<CompetitionApply, CompetitionApplyDto>()
+		.ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+		.ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+		
+		.ForMember(dest => dest.NumLikes, opt => opt.MapFrom(src => src.NumLikes));
+
+		CreateMap<CompetitionApplyDto, CompetitionApply>().IncludeAllDerived()
+
+   .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+   .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+	
+
+   .ForMember(dest => dest.NumLikes, opt => opt.MapFrom(src => src.NumLikes));
+
+
+
+	}
+
 }
