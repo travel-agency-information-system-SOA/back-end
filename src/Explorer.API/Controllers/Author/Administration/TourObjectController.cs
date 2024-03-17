7f00350f099ef4911d0c9a8b46fa5933803dc70e
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2024-03-17 21:59:03 +0100

    feat: create tour object

--- src/Explorer.API/Controllers/Author/Administration/TourObjectController.cs
+++ src/Explorer.API/Controllers/Author/Administration/TourObjectController.cs
@@ -1,9 +1,13 @@
 ﻿using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.Domain.Tours;
 using Explorer.Tours.Core.UseCases.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace Explorer.API.Controllers.Author.Administration
 {
@@ -12,8 +16,9 @@ namespace Explorer.API.Controllers.Author.Administration
     public class ObjectController : BaseApiController
     {
         private readonly ITourObjectService _objectService;
+		private readonly HttpClient _httpClient = new HttpClient();
 
-        public ObjectController(ITourObjectService objectService)
+		public ObjectController(ITourObjectService objectService)
         {
             _objectService = objectService;
         }
@@ -26,11 +31,40 @@ namespace Explorer.API.Controllers.Author.Administration
         }
 
         [HttpPost]
-        public ActionResult<TourObjectDto> Create([FromBody] TourObjectDto tourObject)
+        public async Task<ActionResult<TourObjectDto>> Create([FromBody] TourObjectDto tourObject)
         {
-            var result = _objectService.Create(tourObject);
-            return CreateResponse(result);
-        }
+			// Serijalizujemo objekat u JSON
+			string json = JsonConvert.SerializeObject(tourObject);
+			HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			try
+			{
+				// Šaljemo POST zahtev na mikroservis
+				HttpResponseMessage response = await _httpClient.PostAsync("http://localhost:3000/tourObjects/create", content);
+
+				// Proveravamo status odgovora
+				if (response.IsSuccessStatusCode)
+				{
+					// Ako je odgovor uspešan, čitamo sadržaj odgovora
+					string responseContent = await response.Content.ReadAsStringAsync();
+
+					// Deserijalizujemo JSON odgovor u TourDTO objekat
+					TourObjectDto createdObject = JsonConvert.DeserializeObject<TourObjectDto>(responseContent);
+
+					// Vraćamo OK rezultat sa kreiranim turizmom
+					return Ok(createdObject);
+				}
+				else
+				{
+					// Ako je došlo do greške, vraćamo odgovarajući HTTP status
+					return StatusCode((int)response.StatusCode, "Error occurred while creating tour.");
+				}
+			}
+			catch (HttpRequestException ex)
+			{
+				// Uhvatamo eventualne greške prilikom slanja zahteva
+				return StatusCode(500, $"Error occurred while sending request: {ex.Message}");
+			}
+		}
 
         [HttpPut("{id:int}")]
         public ActionResult<TourObjectDto> Update([FromBody] TourObjectDto tourObject)
