26bca50f08c42c26412b2d8e9e12762202310196
zelick <kristinazelic0908@gmail.com>
2024-05-27 00:32:51 +0200

    feat: Finish translation grpc followers

--- src/Explorer.API/Controllers/FollowerProtoController.cs
+++ src/Explorer.API/Controllers/FollowerProtoController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using GrpcServiceTranscoding;
 using Explorer.Blog.Core.UseCases;
 using NuGet.Packaging;
+using System.Xml.Linq;
+using System;
 
 namespace Explorer.API.Controllers
 {
@@ -22,14 +24,17 @@ namespace Explorer.API.Controllers
 		public override async Task<GrpcServiceTranscoding.NeoFollowerDto> CreateNewFollowing(GrpcServiceTranscoding.UserFollowingDto following,
 			ServerCallContext context)
 		{
+			Console.WriteLine("USAO JE U CREATE NEW FOLLOWER");
 			var httpHandler = new HttpClientHandler();
 			httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 			var channel = GrpcChannel.ForAddress("http://followers:8090", new GrpcChannelOptions { HttpHandler = httpHandler });
 
 			var client = new Follower.FollowerClient(channel);
 			var response = await client.CreateNewFollowingAsync(following);
+            Console.WriteLine("OVO JE RESPONSEEE");
+            Console.WriteLine(response);
 
-			return await Task.FromResult(new GrpcServiceTranscoding.NeoFollowerDto
+            return await Task.FromResult(new GrpcServiceTranscoding.NeoFollowerDto
 			{
 				UserId = response.UserId,
 				Username = response.Username,
@@ -59,6 +64,7 @@ namespace Explorer.API.Controllers
 		public override async Task<ListBlogPostDto> GetFollowingsWithBlogs(id id,
 		 ServerCallContext context)
 		{
+			Console.WriteLine("Usao je GetFollowingsWithBlogs!!!!!!!!!!!");
 			var httpHandler = new HttpClientHandler();
 			httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 			var channel = GrpcChannel.ForAddress("http://followers:8090", new GrpcChannelOptions { HttpHandler = httpHandler });
@@ -70,15 +76,38 @@ namespace Explorer.API.Controllers
 			foreach (var follower in followers.ResponseList)
 			{
 				var blogsResult = _blogPostService.GetAllByAuthorIds(follower.Id);
+                Console.WriteLine("valueeeee");
+                Console.WriteLine(blogsResult.Value);
 
-				if (blogsResult.IsSuccess) //proveriti
-				{
+				//if (blogsResult.IsSuccess) //proveriti
+				//{
 					var blogs = blogsResult.Value;
-					allBlogPosts.AddRange((IEnumerable<BlogPostDto>)blogs);
-				}
+
+					foreach (var blogPost in blogs)
+					{
+						var newBlogPost = new GrpcServiceTranscoding.BlogPostDto
+						{
+							Id = blogPost.Id,
+							AuthorId = blogPost.AuthorId,
+							TourId = blogPost.TourId,
+                            AuthorUsername = blogPost.AuthorUsername ?? "",
+                            Title = blogPost.Title,
+							Description = blogPost.Description,
+							CreationDate = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(blogPost.CreationDate), // Convert DateTime to Timestamp
+																														 //ImageURLs = new List<string>(blogPost.ImageURLs), // Create a new List to make it mutable
+																														 //Ratings = new List<BlogPostRatingDto>(blogPost.Ratings), // Create a new List to make it mutable
+																														 //Comments = new List<BlogPostCommentDto>(blogPost.Comments), // Create a new List to make it mutable
+							Status = blogPost.Status,
+						};
+						allBlogPosts.Add(newBlogPost);
+					}
+				//}
 			}
 
-			return await Task.FromResult(new ListBlogPostDto
+            Console.WriteLine("BLOG POSTTT");
+            Console.WriteLine(allBlogPosts);
+
+            return await Task.FromResult(new ListBlogPostDto
 			{
 				ResponseList = { allBlogPosts }
 			});
